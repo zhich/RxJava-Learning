013af54f87e75de7008803a7e0ab70f6a309a3bb
zhicaihuang <1292984549@qq.com>
2018-06-01 14:51:36 +0800

    组合 / 合并 操作符

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # rxjava-learn
+
+## getting-started
+
+[学习教程来自此处](https://github.com/Carson-Ho/RxJava_Util)
+
+- [入门介绍](https://github.com/zhich/rxjava-learn/blob/master/getting-started/src/main/java/com/zch/rxjava/getstarted/GetStartedActivity.java)
+  > create、just、from ……
+
+- [创建操作符](https://github.com/zhich/rxjava-learn/blob/master/getting-started/src/main/java/com/zch/rxjava/getstarted/CreateOperatorsActivity.java)
+  > create、just、fromArray、fromIterable、empty、error、never、defer、timer、interval、intervalRange、range、rangeLong
+
+- [变换操作符](https://github.com/zhich/rxjava-learn/blob/master/getting-started/src/main/java/com/zch/rxjava/getstarted/ConvertOperatorsActivity.java)
+  > map、flatMap、concatMap、buffer
+
+- [组合 / 合并操作符](https://github.com/zhich/rxjava-learn/blob/master/getting-started/src/main/java/com/zch/rxjava/getstarted/CombinedMergeOperatorsActivity.java)
+  > concat、concatArray、merge、mergeArray、concatDelayError、mergeDelayError、zip、combineLatest、combineLatestDelayError、reduce、collect、startWith、startWithArray、count
+
+- [网络请求轮询（无条件）](https://github.com/zhich/rxjava-learn/blob/master/getting-started/src/main/java/com/zch/rxjava/getstarted/unconditional_network_request_polling/GetTranslationActivity.java)
+  > interval
+
+- [网络请求嵌套回调](https://github.com/zhich/rxjava-learn/blob/master/getting-started/src/main/java/com/zch/rxjava/getstarted/nesting_network_request/UserActivity.java)
+  > flatMap
--- getting-started/src/main/java/com/zch/rxjava/getstarted/CombinedMergeOperatorsActivity.java
+++ getting-started/src/main/java/com/zch/rxjava/getstarted/CombinedMergeOperatorsActivity.java
@@ -4,6 +4,8 @@ import android.util.Log;
 
 import com.zch.libbase.base.BaseActivity;
 
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
@@ -12,7 +14,9 @@ import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.BiConsumer;
 import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
 /**
@@ -47,6 +51,30 @@ public class CombinedMergeOperatorsActivity extends BaseActivity {
         }
     };
 
+    Observer mObserverLong = new Observer<Long>() {
+        @Override
+        public void onSubscribe(Disposable d) { // 默认最先调用复写的 onSubscribe()
+            Log.e(TAG, "开始采用 subscribe 连接");
+        }
+
+        @Override
+        public void onNext(Long l) {
+            Log.e(TAG, "接收到了事件 " + l);
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            Log.e(TAG, "对 Error 事件作出响应---" + e.getMessage());
+        }
+
+        @Override
+        public void onComplete() {
+            Log.e(TAG, "对 Complete 事件作出响应");
+            Log.e(TAG, "----------------------------------------------------------------------------------------");
+        }
+    };
+
+
     Observer mObserverString = new Observer<String>() {
         @Override
         public void onSubscribe(Disposable d) { // 默认最先调用复写的 onSubscribe()
@@ -80,7 +108,11 @@ public class CombinedMergeOperatorsActivity extends BaseActivity {
 //        concat_concatArray(); // 组合多个被观察者，合并后按发送顺序【串行执行】
 //        merge_mergeArray(); // 组合多个被观察者，合并后按“时间线”【并行执行】
 //        concatDelayError_mergeDelayError(); // 推迟发送 onError 事件
-        zip();
+//        zip();
+//        combineLatest();
+//        combineLatestDelayError(); // 作用类似于 concatDelayError() / mergeDelayError() ，即错误处理
+//        reduce(); // 把被观察者需要发送的事件聚合成 1 个事件 & 发送
+        collect(); // 将被观察者 Observable 发送的数据事件收集到一个数据结构里
     }
 
     private void concat_concatArray() {
@@ -226,9 +258,123 @@ public class CombinedMergeOperatorsActivity extends BaseActivity {
         }).subscribe(mObserverString);
 
         /**
+         * 被观察者 1 发送了事件 1
+         * 被观察者 2 发送了事件 A
+         * 接收到了事件 1A
+         * 被观察者 1 发送了事件 2
+         * 被观察者 2 发送了事件 B
+         * 接收到了事件 2B
+         * 被观察者 1 发送了事件 3
+         * 被观察者 2 发送了事件 C
+         * 接收到了事件 3C
+         * 被观察者 2 发送了事件 D
+         */
+
+        /**
          * 特别注意：
          * 1、尽管 observable2 的事件 D 没有事件与其合并，但还是会继续发送
          * 2、若在 observable1 & observable2 的事件序列最后发送 onComplete() 事件，则 observable2 的事件 D 也不会发送
          */
+    }
+
+    private void combineLatest() {
+        /**
+         * 作用：
+         * 当两个 Observables 中的任何一个发送了数据后，将先发送了数据的 Observables 的最新（最后）一个数据与
+         * 另外一个 Observable 发送的每个数据结合，最终基于该函数的结果发送数据
+         * 注意：
+         * 与 zip() 的区别：zip() = 按个数合并，即 1 对 1 合并；CombineLatest() = 按时间合并，即在同一个时间点上合并
+         */
+        Observable.combineLatest(
+                Observable.just(1L, 2L, 3L), // 第 1 个发送数据事件的 Observable
+                Observable.intervalRange(0, 3, 1, 1, TimeUnit.SECONDS), // 第 2 个发送数据事件的 Observable：从 0 开始发送、共发送 3 个数据、第 1 次事件延迟发送时间 = 1s、间隔时间 = 1s
+                new BiFunction<Long, Long, Long>() {
+                    @Override
+                    public Long apply(Long o1, Long o2) throws Exception {
+                        // o1 = 第 1 个 Observable 发送的最新（最后）1 个数据
+                        // o2 = 第 2 个 Observable 发送的每 1 个数据
+                        Log.e(TAG, "合并的数据是：" + o1 + " " + o2);
+                        // 合并的逻辑 = 相加
+                        // 即第 1 个 Observable 发送的最后 1 个数据与第 2 个 Observable 发送的每 1 个数据进行相加
+                        return o1 + o2;
+                    }
+                }
+        ).subscribe(mObserverLong);
+
+        /**
+         * 合并的数据是：3 0
+         * 接收到了事件 3
+         * 合并的数据是：3 1
+         * 接收到了事件 4
+         * 合并的数据是：3 2
+         * 接收到了事件 5
+         */
+    }
+
+    private void combineLatestDelayError() {
+
+    }
+
+    private void reduce() {
+        /**
+         * 作用：
+         * 把被观察者需要发送的事件聚合成 1 个事件 & 发送
+         * 注意：
+         * 聚合的逻辑根据需求撰写，但本质都是前 2 个数据聚合，然后与后 1 个数据继续进行聚合，依次类推
+         */
+
+        Observable.just(1, 2, 3, 4)
+                .reduce(new BiFunction<Integer, Integer, Integer>() {
+                    // 在该复写方法中复写聚合的逻辑
+                    @Override
+                    public Integer apply(Integer s1, Integer s2) throws Exception {
+                        Log.e(TAG, "本次计算的数据是： " + s1 + " 乘 " + s2);
+                        // 本次聚合的逻辑是：全部数据相乘起来
+                        // 原理：第 1 次取前 2 个数据相乘，之后每次获取到的数据 = 返回的数据 x 原始下 1 个数据
+                        return s1 * s2;
+                    }
+                }).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                Log.e(TAG, "最终计算的结果是： " + integer);
+            }
+        });
+
+        /**
+         * 本次计算的数据是： 1 乘 2
+         * 本次计算的数据是： 2 乘 3
+         * 本次计算的数据是： 6 乘 4
+         * 最终计算的结果是： 24
+         */
+    }
+
+    private void collect() {
+        /**
+         * 作用：
+         * 将被观察者 Observable 发送的数据事件收集到一个数据结构里
+         */
+        Observable.just(1, 2, 3, 4, 5, 6)
+                .collect(
+                        // 1. 创建数据结构（容器），用于收集被观察者发送的数据
+                        new Callable<ArrayList<Integer>>() {
+                            @Override
+                            public ArrayList<Integer> call() throws Exception {
+                                return new ArrayList<>();
+                            }
+                            // 2. 对发送的数据进行收集
+                        }, new BiConsumer<ArrayList<Integer>, Integer>() {
+                            @Override
+                            public void accept(ArrayList<Integer> list, Integer integer) throws Exception {
+                                // 参数说明：list = 容器，integer = 后者数据
+                                list.add(integer);
+                                // 对发送的数据进行收集
+                            }
+                        }).subscribe(new Consumer<ArrayList<Integer>>() {
+            @Override
+            public void accept(ArrayList<Integer> list) throws Exception {
+                Log.e(TAG, "本次发送的数据是： " + list);
+
+            }
+        });
     }
 }
